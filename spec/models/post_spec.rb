455af8bfe5bc8975035ea2e313e4a81f213aec9d
zach beecher <zachbeecher@gmail.com>
2017-09-09 21:06:03 -0700

    create index view with BDD and capybara

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,11 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
   describe "Creation" do
-    it 'can be created' do
-      post = Post.create(date: Date.today, rationale: "Anything")
-      expect(post).to be_valid
-    end
+  	before do
+  		@post = FactoryGirl.create(:post)
+  	end
+
+  	it 'can be created' do	
+  		expect(@post).to be_valid
+  	end
+
+  	it 'cannot be created without a date, work_performed, and daily_hours' do
+  		@post.date = nil
+      @post.work_performed = nil
+  		@post.daily_hours = nil
+  		expect(@post).to_not be_valid
+  	end
   end
 end
