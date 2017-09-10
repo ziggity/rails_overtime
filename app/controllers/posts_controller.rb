2e3f0c9aaea77fc5f35cbda7c7ea45c7859cbfc1
zach beecher <zachbeecher@gmail.com>
2017-09-09 22:34:27 -0700

    red green refactor

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,14 +7,20 @@ class PostsController < ApplicationController
     end
 
     def create 
-        @post = Post.new(params.require(:post).permit(:date, :rationale))
-        @post.save
-
-        redirect_to @post
+        @post = Post.new
+        if @post.save
+        redirect_to @post, notice: 'Your post was created'
+        else
+            render :new
+        end
     end
 
     def show
         @post = Post.find(params[:id])
     end
+
+    private 
+        def post_params
+           (params.require(:post).permit(:date, :rationale))
 
 end
