f9d5713e6783df1e17b353fb2840d97176fb222d
zach beecher <zachbeecher@gmail.com>
2017-09-11 11:05:24 -0700

    refactored index page with partials

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -1,26 +1,55 @@
 require 'rails_helper'
 
-RSpec.describe Post, type: :model do
-  describe "Creation" do
+describe 'navigate' do
+  before do
+    @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+    login_as(@user, :scope => :user)
+  end
+
+  describe 'index' do
+    before do
+      visit posts_path
+    end
+
+  	it 'can be reached successfully' do
+  		expect(page.status_code).to eq(200)
+  	end
+
+  	it 'has a title of Posts' do
+  		expect(page).to have_content(/Posts/)
+  	end
+
+    it 'has a list of posts' do
+      post1 = Post.create(date: Date.today, rationale: "Post1", user_id: @user.id)
+      post2 = Post.create(date: Date.today, rationale: "Post2", user_id: @user.id)
+      visit posts_path
+      expect(page).to have_content(/Post1|Post2/)
+    end
+  end
+
+  describe 'creation' do
   	before do
-  		user = User.create(email: 'test@test.com', password 'asdfasdf', password_confirmation: 'asdfasdf', first_name: 'Zach', last_name: 'Beecher')
-			visit new_post_path
-		end
-	end
-  	it 'can be created' do	
-  		expect(@post).to be_valid
+  		visit new_post_path
   	end
 
-  	it 'cannot be created without a date, work_performed, and daily_hours' do
-  		@post.date = nil
-      @post.work_performed = nil
-  		@post.daily_hours = nil
-  		expect(@post).to_not be_valid
+  	it 'has a new form that can be reached' do
+  		expect(page.status_code).to eq(200)
   	end
 
-    it 'has daily hours greater than 0.0' do
-      @post.daily_hours = 0.0
-      expect(@post).to_not be_valid
+  	it 'can be created from new form page' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "Some rationale"
+      click_on "Save"
+
+      expect(page).to have_content("Some rationale")
+  	end
+
+    it 'will have a user associated it' do
+      fill_in 'post[date]', with: Date.today
+      fill_in 'post[rationale]', with: "User Association"
+      click_on "Save"
+
+      expect(User.last.posts.last.rationale).to eq("User Association")
     end
   end
 end
